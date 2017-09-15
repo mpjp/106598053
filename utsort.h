51dd6d1151a0fa8c3839031e8f56ba892ea08656
ingridchen <ingridchen82@gmail.com>
2017-09-15 13:51:54 +0800

    hiuh

--- utsort.h
+++ utsort.h
@@ -2,6 +2,7 @@
 #define UTSORT_H_INCLUDED
 #include <list>
 #include <algorithm>
+#include <cmath>
 template <class Type>
 void swap (Type &a, Type &b){
     Type temp;
@@ -16,7 +17,7 @@ public:
     IncreasingByDistanceToN(int i):n(i){}
     int n;
     bool operator() (int i, int j){
-        return abs(i - n) < abs(j - n);
+        return std::abs(i - n) < std::abs(j - n);
     }
 };
 
@@ -27,7 +28,7 @@ public:
     DecreasingByDistanceToN(Type i):n(i){}
     Type n;
     bool operator() (Type i, Type j){
-        return abs(i - n) > abs(j - n);
+        return std::abs(i - n) > std::abs(j - n);
     }
 };
 
@@ -127,7 +128,7 @@ TEST (Bubble, increasingLambda){
 TEST (Bubble, DecreasingByDistanceToNLambda){
     int a[5]= {5,2,43,6,9};
     int n = 5;
-    bubble (a, 5, [n](int a, int b){return abs(a-n) > abs(b-n);});
+    bubble (a, 5, [n](int a, int b){return std::abs(a-n) > std::abs(b-n);});
 
     ASSERT_EQ(43, a[0]);
     ASSERT_EQ(9, a[1]);
